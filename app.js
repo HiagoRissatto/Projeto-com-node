3f7d2d5ea9a690c5e00a4ca838d20dec0c704f02
HiagoDz7 <levexhiago@gmail.com>
2026-03-08 01:19:38 -0300

    criando as partials e colocando mais codigo em app,.js

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const path = require('path')
 let app = express();
 
 app.set('views', path.join(__dirname, 'views'))
-
 app.engine('handlebars', expHandlebars.engine())
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine("handlebars",expHandlebars.engine({
+    partialsDir: ["views/partials/"]  
+}));
+
 aplicacao.set('view engine', 'handlebars')
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('livros')
 })
 
 app.listen(8000, () => {
